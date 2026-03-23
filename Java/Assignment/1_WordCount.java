b3ce7bf10b5bff7d3e3e51df419d747baad318ab
Gajendra Rathod <145900530+Gajendra182003@users.noreply.github.com>
2026-03-23 17:17:25 +0530

    Add files via upload

---
+++ Java/Assignment/1_WordCount.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class WordCount {
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+            int count=0;
+        while (sc.hasNext()){
+            sc.next();
+            count++;
+        }
+        System.out.println(count);
+        sc.close();
+    }
+}
